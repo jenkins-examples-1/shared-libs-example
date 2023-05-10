d145bdd5c1d47e1ff2ab983378a26ee4a038edd5
Андрей Поляков <AVPGenium@yandex.ru>
2023-05-10 10:19:30 +0300

    Update shellExample.groovy

--- vars/shellExample.groovy
+++ vars/shellExample.groovy
@@ -2,5 +2,6 @@ def call() {
  def result =  libraryResource 'test.sh'
  echo result
  writeFile file: "test.sh", text: result, encoding: "UTF-8"
+ sh "chmod +x ${WORKSPACE}/test.sh"
  sh "${WORKSPACE}/test.sh"
 }
